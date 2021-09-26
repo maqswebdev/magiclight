3f5e2c2ff27d6a56c1d67d70ebe37d643723707e
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-09-26 19:04:28 +0600

    add header

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,20 @@
 //import $ from 'jquery'; window.jQuery = $; window.$ = $ // import module example (npm i -D jquery)
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", function (e) {
+    if (!e.target.classList.contains("link")) {
+      document.querySelectorAll(".main-nav__item ").forEach((link) => {
+        link.classList.remove("active");
+      });
+    }
+  });
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Your JS Code
-
-})
+  $(".main-nav__item.has-child").on("mouseenter", function () {
+    setTimeout(() => {
+      $(".main-nav__item")
+        .removeClass("active")
+        .eq($(this).index())
+        .addClass("active");
+    }, 200);
+  });
+});
