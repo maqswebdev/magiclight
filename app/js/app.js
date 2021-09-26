5bf3f81be20ce3139e2610d19d05afacf3c4baba
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-09-26 20:59:35 +0600

    adapted header

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // Hamburger Menu
+  const $hamburgerBtn = document.querySelector(".hamburger");
+  const $mainNav = document.querySelector(".main-nav");
+  if ($hamburgerBtn) {
+    $hamburgerBtn.addEventListener("click", function () {
+      $mainNav.classList.toggle("active");
+      /*if (this.classList.contains("is-active")) {
+        this.classList.remove("is-active");
+        $mainNav.classList.remove("active");
+        document.body.style.overflow = "";
+      } else {
+        this.classList.add("is-active");
+        $mainNav.classList.add("active");
+        document.body.style.overflow = "hidden";
+      }*/
+    });
+  }
+  const $mainNavClose = document.querySelector(".main-nav__close");
+  if ($mainNavClose) {
+    $mainNavClose.addEventListener("click", function () {
+      $mainNav.classList.toggle("active");
+    });
+  }
+
   $(".main-nav__item.has-child").on("mouseenter", function () {
     setTimeout(() => {
       $(".main-nav__item")
