d52bc88f846d11d98657cd66bf90957f1e7d6254
jokeejoke <69756862+jokeejoke@users.noreply.github.com>
2021-09-29 18:33:08 +0600

    add script for lazyload images

--- app/js/app.js
+++ app/js/app.js
@@ -50,8 +50,62 @@ document.addEventListener("DOMContentLoaded", () => {
       lazyVideoObserver.observe(lazyVideo);
     });
   }
-
   /** END VIDEO LAZYLOAD */
+
+  /** LazyLoad Images */
+  const $lazyImages = document.querySelectorAll(
+    "img[data-src],source[data-srcset]"
+  );
+  const windowHeight = document.documentElement.clientHeight;
+
+  let lazyImagesPositions = [];
+  if ($lazyImages.length) {
+    $lazyImages.forEach((lazyImg) => {
+      if (lazyImg.dataset.src || lazyImg.dataset.srcset) {
+        lazyImagesPositions.push(
+          lazyImg.getBoundingClientRect().top + pageYOffset
+        );
+        lazyScrollCheck();
+      }
+    });
+  }
+
+  window.addEventListener("scroll", lazyScroll);
+
+  function lazyScroll() {
+    if (
+      document.querySelectorAll("img[data-src],source[data-srcset]").length > 0
+    ) {
+      lazyScrollCheck();
+    }
+  }
+
+  function lazyScrollCheck() {
+    let imgIndex = lazyImagesPositions.findIndex(
+      (imgItem) => pageYOffset > imgItem - windowHeight
+    );
+    if (imgIndex >= 0) {
+      if ($lazyImages[imgIndex].dataset.src) {
+        $lazyImages[imgIndex].src = $lazyImages[imgIndex].dataset.src;
+        $lazyImages[imgIndex].removeAttribute("data-src");
+        setTimeout(() => {
+          $lazyImages[imgIndex].parentElement.parentElement.classList.remove(
+            "loading"
+          );
+        }, 200);
+      } else if ($lazyImages[imgIndex].dataset.srcset) {
+        $lazyImages[imgIndex].srcset = $lazyImages[imgIndex].dataset.srcset;
+        $lazyImages[imgIndex].removeAttribute("data-srcset");
+        setTimeout(() => {
+          $lazyImages[imgIndex].parentElement.parentElement.classList.remove(
+            "loading"
+          );
+        }, 200);
+      }
+      delete lazyImagesPositions[imgIndex];
+    }
+  }
+  /** End LazyLoad Images */
 
   // Hamburger Menu
   const $burgerBtn = document.querySelector(".hamburger");
